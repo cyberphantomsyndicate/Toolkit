761be6a449710544ca66759c63ba4825594e8135
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-02-20 21:15:04 -0800

    Update toolkit.py

--- toolkit.py
+++ toolkit.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python3
+import time #line:3
+from colorama import Fore ,Back ,Style ,init #line:4
+init (autoreset =True )#line:5
+def startMessage ():#line:7
+    OO0O0OO0OOO0OO0O0 =input (Fore .YELLOW +"Enter Code To Unlock The Tool : ")#line:8
+    OOOO0OO000OO0OOOO ="qadirahmad"#line:9
+    if OOOO0OO000OO0OOOO !=OO0O0OO0OOO0OO0O0 :#line:10
+        print (Fore .RED +'[X] Wrong Code')#line:11
+        print (Fore .BLUE +''' 
+   1. Go to Insta and massage 
+   2. Insta ID: termux_official078
+   3. Send massage for code
+   4. You need to purchase the code price: 100 rupees
+   5. Next time come with code and use this tool
+    ''')#line:18
+        startMessage ()#line:19
+    else :#line:20
+        print (Fore .GREEN +"Successfully Unlocked Tool!")#line:21
+        pass #line:22
+if __name__ =="__main__":#line:24
+    startMessage ()#line:25
 import os
 import sys
 import re
